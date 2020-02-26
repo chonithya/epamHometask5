533881929e439998f166a0da3a95346ff462ccec
chonithya <51079543+chonithya@users.noreply.github.com>
2020-02-26 13:44:09 +0530

    Add files via upload

---
+++ hometask5/src/main/java/hometask5/SimpleCompoundInterestMain.java
@@ -0,0 +1,10 @@
+package hometask5;
+
+import hometask5.SimpleCompoundInterest;
+
+public class SimpleCompoundInterestMain extends SimpleCompoundInterest{
+	public static void main(String args[]) {
+	    SimpleCompoundInterest logic = new SimpleCompoundInterest();
+	    logic.calculate();
+	}
+}
